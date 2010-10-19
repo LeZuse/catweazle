3e1870859e64fbe5819a30edeb9cbc1934770041
Marcel <mhrdina@hama.co.uk>
2010-10-19 13:23:24 +0100

    EADME & utility scripts

---
+++ hama/build_documentation.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd documentation
+make clean html
---
+++ hama/run_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# ../../bin/nosetests v --with-doctest --doctest-tests
+../../bin/nosetests -v --with-doctest --doctest-tests
